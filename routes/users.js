36b6dff6b22c800a11eb66beb1d40a920f718e76
thanhngancao <thanhngancao83@gmail.com>
2020-06-01 00:00:43 +0700

    add

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,7 @@ const passport = require('passport');
 // Load User model
 const User = require('../models/User');
 const Comment = require('../models/Comment');
-var MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://b6:y6bVkAtNvFsbKOeo@cluster0-xcap6.gcp.mongodb.net/b6?retryWrites=true&w=majority';
+
 
 const { forwardAuthenticated,ensureAuthenticated } = require('../config/auth');
 
