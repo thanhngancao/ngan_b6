ecd3350736e3347781006c4b5f37df036f7a3932
thanhngancao <thanhngancao83@gmail.com>
2020-06-01 10:02:26 +0700

    fix

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ mongoose
 //Set Header
 app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers','Content-Type, api_key');
   next();
 });
